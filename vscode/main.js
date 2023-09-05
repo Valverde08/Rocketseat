ebdc5fc49fdcc630cd03720450599c020bb424b8
Valverde08 <120853222+Valverde08@users.noreply.github.com>
2023-09-04 23:10:40 -0300

    finalização quarto capitulo

---
+++ vscode/main.js
@@ -0,0 +1,18 @@
+function toggleMode(){
+    const body = document.body
+    body.classList.toggle("light")
+   
+
+    const img = document.querySelector("#profile img")
+
+    if(body.classList.contains("light")){
+        
+        img.setAttribute("src ","./imagens/avatar-light.png")
+
+        
+    } else {
+        
+        img.setAttribute("src ","./imagens/avatar.png")
+    }
+
+}
